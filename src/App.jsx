601b71ecc8dcc5e120d189327bd1ef234d8bb2cb
Lauren Champs <laurenkchamps@gmail.com>
2024-04-28 10:19:45 +0930

    Fetch country data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,52 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import React from 'react';
+
+// `https://restcountries.com/v3.1/all?fields=cca3,name,flags`
 
 const tempCountries = [
   {
     id: 1,
     name: 'Australia',
-    abbrev: 'AUS',
+    cca3: 'AUS',
   },
   {
     id: 2,
     name: 'Palestine',
-    abbrev: 'PAL',
+    cca3: 'PAL',
   },
   {
     id: 3,
     name: 'Yemen',
-    abbrev: 'YEM',
+    cca3: 'YEM',
   },
 ];
 
+const NUMBER_OF_COUNTRIES = 8;
+
 function App() {
-  const [countries, setCountries] = useState(tempCountries);
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    async function fetchCountries() {
+      const res = await fetch(
+        `https://restcountries.com/v3.1/all?fields=cca3,name,flag`
+      );
+
+      const data = await res.json();
+
+      console.log(data);
+
+      let selectedCountries = [];
+
+      for (let i = 0; i < NUMBER_OF_COUNTRIES; i++) {
+        const randomNum = Math.floor(Math.random() * 250) + 1;
+        selectedCountries.push(data[randomNum]);
+      }
+
+      setCountries(selectedCountries);
+    }
+    fetchCountries();
+  }, []);
 
   return (
     <>
@@ -38,7 +64,7 @@ function CountryList({ countries }) {
   return (
     <ul className="container list list-countries">
       {countries.map((country) => (
-        <Country country={country} key={country.id} />
+        <Country country={country} key={country.cca3} />
       ))}
     </ul>
   );
@@ -47,8 +73,11 @@ function CountryList({ countries }) {
 function Country({ country }) {
   return (
     <li className="country">
-      <h2>{country.name}</h2>
-      <p>{country.abbrev}</p>
+      <div className="country-name">
+        <h2>{country.name.common}</h2>
+        <p>{country.cca3}</p>
+      </div>
+      <p className="flag">{country.flag}</p>
     </li>
   );
 }
